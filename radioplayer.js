91ccd823fbae262c0a99dffda6df21d4f83d0262
OlleLeonard <olleleonard@gmail.com>
2023-11-23 16:36:01 +0100

    svarat på frågor

--- radioplayer.js
+++ radioplayer.js
@@ -1,8 +1,3 @@
-// Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
-
-
-// Make a GET request to the API
-
 const parentElement = document.getElementById("channels");
 
 async function fetchJson(){
@@ -13,17 +8,17 @@ console.log(data)
 
     data.channels.forEach((radiokanal) => {
         console.log(radiokanal);
-        const audioTag = document.createElement("div");
-        audioTag.className = "styling";
-        audioTag.innerHTML = `<img src="${radiokanal.image}"/>
+        const sound = document.createElement("div");
+        sound.className = "styling";
+        sound.innerHTML = `<img src="${radiokanal.image}"/>
         <div>
         <h2>${radiokanal.name}</h2>
         <audio controls>
         <source src= "${radiokanal.liveaudio.url}" type="audio/mpeg" />
         </audio>
         </div>`
-        audioTag.style.backgroundColor = `#${radiokanal.color}`;
-        parentElement.appendChild(audioTag);
+        sound.style.backgroundColor = `#${radiokanal.color}`;
+        parentElement.appendChild(sound);
 });
 
 };
